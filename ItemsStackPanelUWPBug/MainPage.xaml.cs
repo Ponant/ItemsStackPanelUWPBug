8f74dbe3d08f9d38d73ca8aa0d7384b2dc689de3
Ponant <ponant@yombo.com>
2020-01-22 11:33:15 +0100

    Simplified again

--- ItemsStackPanelUWPBug/MainPage.xaml.cs
+++ ItemsStackPanelUWPBug/MainPage.xaml.cs
@@ -19,14 +19,12 @@ namespace ItemsStackPanelUWPBug
         {
             this.InitializeComponent();
             Items = new ObservableCollection<string>();
-            for (int i = 0; i < 24000; i++)
+            for (int i = 0; i < 30000; i++)//Increase length here
             {
                 Items.Add($"Item 1");
             }
         }
-
         public ObservableCollection<string> Items { get; }
-
     }
 }
 
