8ce3d573a08d2ad5f3bb9a651ab95626eaf989ed
Ponant <ponant@yombo.com>
2020-01-22 11:02:03 +0100

    Simplified problem

--- ItemsStackPanelUWPBug/MainPage.xaml.cs
+++ ItemsStackPanelUWPBug/MainPage.xaml.cs
@@ -19,65 +19,13 @@ namespace ItemsStackPanelUWPBug
         {
             this.InitializeComponent();
             Items = new ObservableCollection<string>();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 24000; i++)
             {
                 Items.Add($"Item 1");
             }
-            CollectionViewSource = new CollectionViewSource();
-            WorksCollectionViewSource = new CollectionViewSource();
         }
-
-        private StorageFolder _folder;
-        private QueryOptions _queryOptions;
-        private StorageFileQueryResult _query;
-        private FileInformationFactory _fileInformationFactory;
-
-        public CollectionViewSource CollectionViewSource { get; set; }
-
-        public CollectionViewSource WorksCollectionViewSource { get; set; }
 
         public ObservableCollection<string> Items { get; }
-
-        private async void FolderPickerButton_Click(object sender, RoutedEventArgs e)
-        {
-            var _pickedFolder = await PickFolderAsync();
-            if (_pickedFolder == null)
-            {
-                return;
-            }
-
-            _folder = _pickedFolder;
-            _queryOptions = new QueryOptions
-            {
-                FolderDepth = FolderDepth.Deep,
-                IndexerOption = IndexerOption.UseIndexerWhenAvailable,
-            };
-
-            _query = _folder.CreateFileQueryWithOptions(_queryOptions);
-
-            _fileInformationFactory = new FileInformationFactory(_query, ThumbnailMode.SingleItem, 160,
-                ThumbnailOptions.UseCurrentScale, delayLoad: false);
-
-            var _vector = _fileInformationFactory.GetVirtualizedFilesVector();
-
-            CollectionViewSource.Source = _vector;
-
-            WorksCollectionViewSource.Source = Items;
-        }
-
-        private static async Task<StorageFolder> PickFolderAsync()
-        {
-            var folderPicker = new FolderPicker
-            {
-                SuggestedStartLocation = PickerLocationId.Desktop,
-                ViewMode = PickerViewMode.Thumbnail
-            };
-
-            folderPicker.FileTypeFilter.Add("*");
-
-            var _pickedFolder = await folderPicker.PickSingleFolderAsync();
-            return _pickedFolder;
-        }
 
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# ItemsStackPanelUWPBug
-https://stackoverflow.com/questions/59850251/itemsstackpanel-extends-beyond-grid-container-when-orientation-is-set-to-horizon
